5b4bb8eaf3b74e8693b0a833b37256002d418e93
lawtronicus <46512764+lawtronicus@users.noreply.github.com>
2023-12-19 10:58:32 +0900

    created basic file structure, added a favoriteFood variable and wrote it to the DOM as per assignment instructions

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert("Hello World");
+
+//declare favorite food variable
+let favoriteFood = "Tacos" //using 'let' for the declaration here as my favorite food depends on the day of the week ; D
+console.log(favoriteFood);
+
+document.write(favoriteFood);
