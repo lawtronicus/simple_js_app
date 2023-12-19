1fe408f886963bfe3cbd66f6f521b084d5c3a128
lawtronicus <46512764+lawtronicus@users.noreply.github.com>
2023-12-19 17:12:43 +0900

    created a for loop that goes through the pokemon object list and writes the name and height to the document along with a comment on the bigness of the pokemon if height is greater than 2

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,15 @@ let pokemonList = [
         types: ['electric'] 
     }
 ];
+
+//Write name and height to document, as well as a note on its bigness
+
+// begin looping through pokemonList
+for (let i=0; i < pokemonList.length; i++) {
+
+    //set variable to '(Wow! That's big!)' if height is greater than 2, and an empty string if not
+    let isItBig = pokemonList[i].height > 2 ? "(Wow! That's big!)" : "";
+
+    //write result to document along with a break for readability
+    document.write(`${pokemonList[i].name} - height: ${pokemonList[i].height} ${isItBig} <br>`);
+};
