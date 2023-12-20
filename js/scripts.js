a5b3e6e17abe82d7ff3909f01e63845ba214fd47
lawtronicus <46512764+lawtronicus@users.noreply.github.com>
2023-12-20 12:04:20 +0900

    wrapped code that was determining if a pokemon was big inside a function and improved it by using the higher order function .map. Also wrapped the code that is writing to the document inside a function and improved it using the higher order function forEach. Added comments for these functions. Tried to keep everything as functional as possible.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,65 @@
-// declare variables
+// create protected pokemon object that cannot be modified
 
-const pokemonList = [
-    {
-        name: 'Bulbasur',
-        height: 2.04,
-        weight: 15.2,
-        category: "Seed",
-        types: ['grass', 'poison'] 
-    },
-    {
-        name: 'Charmander',
-        height: 2.00,
-        weight: 18.7,
-        category: 'Lizard',
-        types: ['fire'] 
-    }, 
-    {
-        name: 'Squirtle',
-        height: 1.08,
-        weight: 19.8,
-        category: 'Tiny Turtle',
-        types: ['water'] 
-    },
-    {
-        name: 'Pikachu',
-        height: 1.04,
-        weight: 13.2,
-        category: 'mouse',
-        types: ['electric'] 
-    }
-];
+const basePokemonList = (function() {
+    const myPokemon = [
+        {
+            name: 'Bulbasur',
+            height: 2.04,
+            weight: 15.2,
+            category: 'Seed',
+            types: ['grass', 'poison'] 
+        },
+        {
+            name: 'Charmander',
+            height: 2.00,
+            weight: 18.7,
+            category: 'Lizard',
+            types: ['fire'] 
+        }, 
+        {
+            name: 'Squirtle',
+            height: 1.08,
+            weight: 19.8,
+            category: 'Tiny Turtle',
+            types: ['water'] 
+        },
+        {
+            name: 'Pikachu',
+            height: 1.04,
+            weight: 13.2,
+            category: 'mouse',
+            types: ['electric'] 
+        }];
+    return myPokemon 
+})();
 
-//Write name and height to document, as well as a note on its bigness
+/**
+ * Adds a 'isBig' property to each pokemon in the array based on their height.
+ * @param {Object[]} pokemonArray - Array of pokemon objects.
+ * @return {Object[]} New array of pokemon objects with 'isBig' property added.
+ */
 
-// begin looping through pokemonList
-for (let i=0; i < pokemonList.length; i++) {
+function addBignessProperty(pokemonArray) {
+    return pokemonArray.map(pokemon => {
+        return {
+            ...pokemon,
+            isBig: pokemon.height > 2
+        };
+    });
+ };
 
-    //set variable to '(Wow! That's big!)' if height is greater than 2, and an empty string if not
-    let isItBig = pokemonList[i].height > 2 ? "(Wow! That's big!)" : "";
+/**
+ * Writes information about each pokemon to the document, including a special note if the pokemon is big.
+ * @param {Object[]} pokemonArray - Array of pokemon objects with 'isBig' property.
+ */
 
-    //write result to document along with a break for readability
-    document.write(`${pokemonList[i].name} - height: ${pokemonList[i].height} ${isItBig} <br>`);
+function writePokemonListToDoc(pokemonArray) {
+    pokemonArray.forEach(pokemon => {
+        const message = pokemon.isBig
+            ? `${pokemon.name} - height: ${pokemon.height} - Wow! That's big! <br>`
+            : `${pokemon.name} - height: ${pokemon.height}<br>`;
+        document.write(message);
+    });
 };
+
+writePokemonListToDoc(addBignessProperty(basePokemonList));
